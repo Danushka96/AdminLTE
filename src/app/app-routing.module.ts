02b7d2b88282d4a35e75eaafd0ae579b40de0200
danushka96 <danushkaherath96@gmail.com>
2019-01-23 19:35:57 +0530

    initial file setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {Dashboard2Component} from './dashboard2/dashboard2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent},
+  { path: 'dashboard2', component: Dashboard2Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [DashboardComponent, Dashboard2Component];
