13858121ad45e580b0c8ee26974f63f6153845fc
danushka96 <danushkaherath96@gmail.com>
2019-01-23 21:25:39 +0530

    add some view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {Dashboard2Component} from './dashboard2/dashboard2.component';
+import {TopNavComponent} from './top-nav/top-nav.component';
+import {BoxedComponent} from './boxed/boxed.component';
+import {WidgetsComponent} from './widgets/widgets.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent},
-  { path: 'dashboard2', component: Dashboard2Component}
+  { path: 'dashboard2', component: Dashboard2Component},
+  { path: 'top-nav', component: TopNavComponent},
+  { path: 'boxed', component: BoxedComponent},
+  { path: 'widgets', component: WidgetsComponent}
 ];
 
 @NgModule({
@@ -13,4 +19,11 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [DashboardComponent, Dashboard2Component];
+
+export const routingComponents = [
+  DashboardComponent,
+  Dashboard2Component,
+  TopNavComponent,
+  BoxedComponent,
+  WidgetsComponent
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { BoxedComponent } from './boxed/boxed.component';
+import { WidgetsComponent } from './widgets/widgets.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    TopNavComponent,
+    BoxedComponent,
+    WidgetsComponent
   ],
   imports: [
     BrowserModule,
